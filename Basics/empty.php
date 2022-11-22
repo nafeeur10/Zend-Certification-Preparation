78378d62d6fa248efccec2b113466fcec1050fd8
Nafeeur Rahman <nafeeur.kuet.cse6@gmail.com>
2022-11-22 12:26:30 +0600

    update empty.php with output

--- Basics/empty.php
+++ Basics/empty.php
@@ -37,3 +37,14 @@ if(empty($boolean)) {
   echo "It's Boolean False";
   echo "<br>";
 }
+
+/*
+Output:
+
+It's null
+It's empty string
+It's empty Array
+It's Zero
+It's Zero String
+It's Boolean False
+*/
